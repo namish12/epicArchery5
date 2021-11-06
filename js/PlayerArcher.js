984fbf225b633dd1f87d3b0915febe0941f5790c
namish12 <89356264+namish12@users.noreply.github.com>
2021-11-06 10:25:03 -0700

    Update PlayerArcher.js

--- js/PlayerArcher.js
+++ js/PlayerArcher.js
@@ -21,12 +21,12 @@ class PlayerArcher {
     console.log(angle)
 
     if (keyIsDown(DOWN_ARROW) && angle < -73 ) {
-      angle += 1;
+      angle += 0.5;
       Matter.Body.setAngle(this.body, angle);
     }
 
     if (keyIsDown(UP_ARROW) && angle > -103) {
-      angle -=1;
+      angle -=0.5;
       Matter.Body.setAngle(this.body, angle);
     }
 
